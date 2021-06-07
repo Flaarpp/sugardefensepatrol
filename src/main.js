08ae3e72c6b0501b53eae6f096f174a298d4fefd
Flaarpp <iellis@ucsc.edu>
2021-06-07 06:43:59 -0700

    updated added rocket

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ let config = {
   width: 640,
   height: 480,
   scene: [ Menu, Play ]
+  
 }
   let game = new Phaser.Game(config);
+  // reserve keyboard vars
+  let keyF, keyR, keyLEFT, keyRIGHT;
   // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
