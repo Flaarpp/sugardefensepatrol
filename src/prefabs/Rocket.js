55f10ceaa7ff1c3b257728009b9a8c8152f031be
Flaarpp <iellis@ucsc.edu>
2021-06-07 08:14:52 -0700

    checking

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,7 +2,7 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
       super(scene, x, y, texture, frame);
-  
+      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
       scene.add.existing(this); // add object to existing scene
       this.isFiring = false;
       this.moveSpeed = 2;
@@ -17,18 +17,24 @@ update() {
             this.x += this.moveSpeed;
         }
     // }
-    //fire button
-    if(Phaser.Input.Keyboard.JustDown(keyF)) {
-        this.isFiring = true;
-    }
+  // fire button
+  if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+    this.isFiring = true;
+    this.sfxRocket.play();  // play sfx
+  }
     //if fired, move up
     if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
         this.y -= this.moveSpeed;
     }
     //reset on miss
     if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+      }
+    }
+
+    //reset rocket to "ground"
+    reset() {
         this.isFiring = false;
         this.y = game.config.height - borderUISize - borderPadding;
-      }
     }
   }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,8 +3,59 @@ class Menu extends Phaser.Scene {
       super("menuScene");
     }
     
-    create() {
-      this.add.text(20, 20, "Rocket Patrol Menu");
-      this.scene.start("playScene");
+    preload() {
+      // load audio
+      this.load.audio('sfx_select', './assets/blip_select12.wav');
+      this.load.audio('sfx_explosion', './assets/explosion38.wav');
+      this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
+
+    create() {
+      let menuConfig = {
+        fontFamily: 'Arial',
+        fontSize: '20px',
+        backgroundColor: '#ff6961',
+        color: '#000',
+        align: 'right',
+        padding: {
+          top: 4,
+          bottom: 6,
+        },
+        fixedWidth: 0
+      }
+
+      
+
+      this.add.text(game.config.width/2, game.config.height/2 - borderUISize - 
+      borderPadding, 'SUGAR PATROL', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2, 'Use <-- --> arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
+      menuConfig.backgroundColor = '#89cff0';
+      menuConfig.color = '#000';
+      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <-- for Novice or --> for Expert', menuConfig).setOrigin(0.5);
+       // define keys
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+      if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+        // easy mode
+        game.settings = {
+          spaceshipSpeed: 3,
+          gameTimer: 60000    
+        }
+        this.sound.play('sfx_select');
+        this.scene.start('playScene');    
+      }
+      if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+        // hard mode
+        game.settings = {
+          spaceshipSpeed: 4,
+          gameTimer: 45000    
+        }
+        this.sound.play('sfx_select');
+        this.scene.start('playScene');    
+      }
+    }
+
   }
