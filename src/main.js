c70eeec0711e2779b59ddd0d0c86289c64d1a6e0
Flaarpp <iellis@ucsc.edu>
2021-06-07 10:06:05 -0700

    comments in main

--- src/main.js
+++ src/main.js
@@ -11,3 +11,6 @@ let config = {
   // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+//Name = Ian Ellis, Title = Sugar Defense Patrol, Date = 6/7/21, Time to Complete = 8 Hours
+//Points Breakdown: 60 = Aesthetic Overhaul, 5 = Moving Rocket
